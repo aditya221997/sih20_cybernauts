fc66aecc9649892ed0d0b5fc96e10737132f59ff
Yogya15023 <69039951+Yogya15023@users.noreply.github.com>
2020-08-03 16:22:26 +0530

    Update home.blade yogya.php

--- home.blade yogya.php
+++ home.blade yogya.php
@@ -15,7 +15,7 @@
     document.addEventListener("DOMContentLoaded", function(){
       navigator.getBattery().then(function(battery){
         var level = battery.level;
-        if(level<0.49)
+        if(level<0.76)
       { alert("Low Battery!! Please Charge Your Phone!!"); }
 
       });
